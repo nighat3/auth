111368870ca1bb850cda50f431c9b28491eb679a
Nighat Ansari <nighat.ansari@kellogg.northwestern.edu>
2025-02-27 03:46:10 +0000

    tweaks

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,11 +1,14 @@
 class ActivitiesController < ApplicationController
 
   def create
-    @activity = Activity.new
-    @activity["contact_id"] = params["contact_id"]
-    @activity["activity_type"] = params["activity_type"]
-    @activity["note"] = params["note"]
-    @activity.save
+    if session["user_id"] != nil
+      @activity = Activity.new
+      @activity["contact_id"] = params["contact_id"]
+      @activity["activity_type"] = params["activity_type"]
+      # @activity["user_id"] = ...
+      @activity["note"] = params["note"]
+      @activity.save
+    end
     redirect_to "/contacts/#{@activity["contact_id"]}"
   end
 
