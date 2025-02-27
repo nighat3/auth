50b23c68fe05e1aa107f030a990754a3db67023d
Nighat Ansari <nighat.ansari@kellogg.northwestern.edu>
2025-02-27 02:33:44 +0000

    class 8

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,12 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+password = "puppies"
+encrypted_password = BCrypt::Password.create("puppies")
+puts encrypted_password
 # 2. prepare encrypted string for testing
+
+puts BCrypt::Password.new(encrypted_password) == "puppies1"
+
 # 3. test secret against prepared encrypted string
+
